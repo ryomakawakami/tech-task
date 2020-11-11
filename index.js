56e9a4254e810dfd66dbda74e5220e2cdede2db7
ryomakawakami <ryomakwkm@gmail.com>
2020-11-11 18:40:21 -0500

    Added login flow

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.listen(port, function() {
-    console.log('Server listening on port 3000')
-})
+app.listen(port, () => console.log('Server listening on port 3000'))
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/auth', (req, res) => {
-    console.log(req)
-    res.redirect(__dirname + '/index.html')
+    var username = req.body.username
+    var password = req.body.password
+
+    // Handle login logic
+    switch (login(username, password)) {
+        case 0:
+            res.write('<p>Hello ' + username + '</p>')
+            res.end('<a href="/">Logout</a>')
+            break;
+        case 1:
+            res.write('<p>Wrong password</p>')
+            res.end('<a href="/">Go back</a>')
+            break;
+        case 2:
+            res.write('<p>User doesn\'t exist</p>')
+            res.end('<a href="/">Go back</a>')
+            break;
+    }
 })
+
+// 0: success, 1: wrong password, 2: user doesn't exist
+function login(username, password) {
+    var users = {
+        'oh': 'io',
+        'marco': 'polo',
+        'hello': 'world'
+    }
+
+    if (username in users) {
+        if (users[username] == password) {
+            return 0;
+        } else {
+            return 1;
+        }
+    } else {
+        return 2;
+    }
+}
